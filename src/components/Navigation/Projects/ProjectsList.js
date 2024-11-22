785a541609b271f2403e1d67216a740985e3749b
i39q <111959426+i39q@users.noreply.github.com>
2024-11-22 21:50:07 +0300

    Update ProjectsList.js
    update links

--- src/components/Navigation/Projects/ProjectsList.js
+++ src/components/Navigation/Projects/ProjectsList.js
@@ -73,7 +73,7 @@ const Projects = [
         "obj" : [
             {
                 "name" : "notabug",
-                "link" :  "https://notabug.org/i39q/hashsum"
+                "link" : "https://notabug.org/i39q/hashsum"
             },
             {
                 "name" : "github",
@@ -87,11 +87,11 @@ const Projects = [
         "obj" : [
             {
                 "name" : "notabug",
-                "link" :  "https://notabug.org/i39q/Training/src/main/Translator"
+                "link" : "https://notabug.org/i39q/Training/src/main/Translator"
             },
             {
                 "name" : "github",
-                "link" : "https://github.com/i39q/Training/src/main/Translator"
+                "link" : "https://github.com/GoneNowhere/Training/tree/main/Translator"
             }
         ]
     }
